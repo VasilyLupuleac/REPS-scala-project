1da7adf8eb0d8bddb6b64737e657839107a2b6be
VasilyLupuleac <v_lupuleac@mail.ru>
2023-05-15 16:15:57 +0300

    Refactor FileSensorStorage to use implicits and change the time format

--- src/main/scala/FileSensorStorage.scala
+++ src/main/scala/FileSensorStorage.scala
@@ -1,46 +1,56 @@
 import java.io.{BufferedWriter, FileWriter}
-import java.time.{LocalDate, LocalDateTime, LocalTime}
+import java.time.{LocalDate, LocalTime}
 import java.time.format.DateTimeFormatter
 import scala.io.Source
 
 
 class FileSensorStorage(val filename: String) extends SensorDataStorage {
-  def toString(reading: SensorReading): String = s"${reading.time.format(DateTimeFormatter.ISO_LOCAL_TIME)} ${reading.date.format(DateTimeFormatter.ISO_LOCAL_DATE)} ${reading.value} ${reading.sensorId} "
 
-  def toInt(s: String): Option[Int] = {
-    try {
-      Some(s.toInt)
-    } catch {
-      case e: Exception => None
-    }
+  private val dateFormat = DateTimeFormatter.ISO_LOCAL_DATE
+  private val timeFormat = DateTimeFormatter.ofPattern("HH:mm")
+  implicit class ReadingToString(reading: SensorReading) {
+    def convertToString() = s"${reading.time.format(timeFormat)} " +
+      s"${reading.date.format(dateFormat)} " +
+      s"${reading.value} ${reading.sensorId}"
   }
 
-    private def fromString(line: String): Option[SensorReading] = {
-      val fields = line.split(" ")
-      val time = LocalTime.parse(fields(0), DateTimeFormatter.ISO_LOCAL_TIME)
-      val date = LocalDate.parse(fields(1), DateTimeFormatter.ISO_LOCAL_DATE)
-      val value = toInt(fields(2)).getOrElse(return None)
-      val sensorId = toInt(fields(3)).getOrElse(return None)
-      Some(SensorReading(time = time,date = date, value = value, sensorId = sensorId))
+  private def stringToReadingOption(line: String): Option[SensorReading] = {
+    val fields = line.split(" ")
+    val time = try {
+      LocalTime.parse(fields(0), timeFormat)
+    } catch {
+      case e: Exception => return None
     }
+    val date = try {
+      LocalDate.parse(fields(1), dateFormat)
+    }
+    catch {
+      case e: Exception => return None
+    }
+
+    val value = fields(2).toIntOption.getOrElse(return None)
+    val sensorId = fields(3).toIntOption.getOrElse(return None)
+    Option(SensorReading(time = time, date = date, value = value, sensorId = sensorId))
+  }
 
 
   override def addReading(entry: SensorReading): Unit = {
     val writer = new BufferedWriter(new FileWriter(filename, true))
     try {
-      writer.write(toString(entry))
+      writer.write(entry.convertToString())
       writer.newLine()
     } finally {
       writer.close()
     }
   }
-    override def getAllReadings(): List[SensorReading] = {
-      val source = Source.fromFile(filename)
-      try {
-        val lines = source.getLines().toList
-        lines.flatMap(fromString)
-      } finally {
-        source.close()
-      }
+
+  override def getAllReadings(): List[SensorReading] = {
+    val source = Source.fromFile(filename)
+    try {
+      val lines = source.getLines().toList
+      lines.flatMap(stringToReadingOption)
+    } finally {
+      source.close()
     }
+  }
 }
